a24e2beadafb6877178fb8b2eecc07bc71de099d
Roman Salazar <romansito18@gmail.com>
2016-11-03 11:01:27 -0700

    getting the usersProfileImage is working!!!!!!!

--- tweeter/ProfileViewController.swift
+++ tweeter/ProfileViewController.swift
@@ -16,32 +16,35 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var numberOfFollowersLabel: UILabel!
     @IBOutlet weak var followingNumberLabel: UILabel!
     
-    var user : User?
+    var user : User? {
+        didSet {
+            if let myUser = user {
+                profileImage(key: myUser.profileImageUrlString, completion: { (image) in
+                    self.profileImageview.image = image
+                })
+            }
+        }
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         UIApplication.shared.statusBarStyle = .lightContent
+       
+
+        
     }
     
     override func viewDidLoad() {
-        setupProfile()
-
         super.viewDidLoad()
         self.title = "PROFILE"
 
-
-        }
-
-    
-    func setupProfile() {
-    
         API.share.getUserAccount { (user) in
             self.user = user
             
             OperationQueue.main.addOperation {
-
-            self.userNameLabel.text = self.user?.name
-            
+                
+                self.userNameLabel.text = self.user?.name
+                
                 if let followers = self.user?.followersCount {
                     self.numberOfFollowersLabel.text = "\(followers)"
                 }
@@ -49,45 +52,28 @@ class ProfileViewController: UIViewController {
                 if let followingNumbers = user?.friendsCount {
                     self.followingNumberLabel.text = "\(followingNumbers)"
                 }
-            
+                
             }
         }
+
     }
-
-
     
-    func profileImage(key: String, completion: (UIImage?)-> ()) {
+    
+    
+    
+    func profileImage(key: String, completion: @escaping (UIImage?) -> ()) {
         if let image = SimpleCache.share.image(key: key) {
             completion(image)
             return
         }
+        
+        API.share.getImage(urlString: key) { (image) -> () in
+            if let image = image {
+                completion(image)
+            }
+        }
     }
-    
-//    func profileImage(key: String, completion: @escaping (UIImage?) -> ())
-//    {
-//        if let image = SimpleCache.share.image(key: key) {
-//            completion(image)
-//            return
-//        }
-//        
-//        API.share.getImage(urlString: key) { (image) -> () in
-//            if let image = image {
-//                completion(image)
-//            }
-//        }
-//    }
-    
-//    func configureBlurImage() {
-//        
-//        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.prominent)
-//        let blurEffectView = UIVisualEffectView(effect: blurEffect)
-//        blurEffectView.frame = view.bounds
-//        blurEffectView.alpha = 0.3
-//        blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth] // for supporting device rotation
-//        view.addSubview(blurEffectView)
-//        
-//    }
-    
+
 
 
 }
